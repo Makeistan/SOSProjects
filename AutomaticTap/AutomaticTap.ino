4ab63de5af1dc4cd295225dd590225bba9b28fe7
Quswar Mahmood Abid <bsee14013@itu.edu.pk>
2016-12-01 19:03:50 +0500

    Automatic Tap

---
+++ AutomaticTap/AutomaticTap.ino
@@ -0,0 +1,31 @@
+#include <Servo.h>
+
+Servo myservo;
+int pos = 0;
+int bottomPin=6;
+int topPin=5;
+
+void setup()
+{
+  myservo.attach(9);
+  pinMode(bottomPin,INPUT);
+  pinMode(topPin,INPUT);
+}
+void loop()
+{
+  if(digitalRead(bottomPin)==LOW && digitalRead(topPin)==LOW)
+  {
+    myservo.write(90);
+  }
+  if(digitalRead(bottomPin)==HIGH && digitalRead(topPin)==HIGH)
+  {
+    myservo.write(0);
+  }
+  if(digitalRead(bottomPin)==HIGH && digitalRead(topPin)==LOW)
+  {
+    myservo.write(90);
+  }
+  
+  //delay(60*1000);
+
+}
