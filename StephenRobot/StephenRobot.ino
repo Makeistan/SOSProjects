345df68d26fcacc8f40dafe3b9bd6c9a5c4d8b24
Quswar Mahmood Abid <bsee14013@itu.edu.pk>
2016-12-01 18:17:48 +0500

    Files Added!

---
+++ StephenRobot/StephenRobot.ino
@@ -0,0 +1,46 @@
+/* Sweep
+ by BARRAGAN <http://barraganstudio.com>
+ This example code is in the public domain.
+
+ modified 8 Nov 2013
+ by Scott Fitzgerald
+ http://www.arduino.cc/en/Tutorial/Sweep
+*/
+
+
+#include <Servo.h>
+
+Servo myservo1;  // create servo object to control a servo
+
+Servo myservo2;  // create servo object to control a servo
+
+Servo myservo3;  // create servo object to control a servo
+
+int potpin = 0;  // analog pin used to connect the potentiometer
+int val;    // variable to read the value from the analog pin
+int pos=0;
+
+
+void setup() 
+{
+  myservo1.attach(9);  // attaches the servo on pin 9 to the servo object
+  myservo2.attach(10);  // attaches the servo on pin 10 to the servo object
+  myservo3.attach(11);  // attaches the servo on pin 11 to the servo object
+}
+
+void loop() {
+  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo1.write(pos);              // tell servo to go to position in variable 'pos'
+    myservo2.write(pos);              // tell servo to go to position in variable 'pos'
+     myservo3.write(pos);            // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo1.write(pos);              // tell servo to go to position in variable 'pos'
+    myservo2.write(pos);              // tell servo to go to position in variable 'pos'
+    myservo3.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+}
+
---
+++ StewardPointer/StewardPointer.ino
@@ -0,0 +1,31 @@
+#include <Servo.h>
+
+Servo ahmed;
+Servo sufiyan;
+
+int x=0;
+int y=0;
+
+void setup() 
+{
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  ahmed.attach(9);
+  sufiyan.attach(10);  
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+  x=analogRead(A0);
+  y=analogRead(A1);
+//  Serial.print(x);
+//  Serial.print(",");
+//  Serial.println(y);
+  x = map(x, 0, 1023, 45, 135);     // sfgfhdgfhgrsefagjsddfghmgsdfghv
+  y = map(y, 0, 1023, 45, 135);     // sfgfhdgfhgrsefagjsddfghmgsdfghv
+  ahmed.write(x);
+  sufiyan.write(y);
+  delay(100);
+  
+}
