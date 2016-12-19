cfa3f8612d09955ac88075055b94acf1f59268e7
Quswar Mahmood Abid <bsee14013@itu.edu.pk>
2016-12-19 17:24:02 +0500

    Update AutomaticTap.ino

--- AutomaticTap/AutomaticTap.ino
+++ AutomaticTap/AutomaticTap.ino
@@ -15,15 +15,11 @@ void loop()
 {
   if(digitalRead(bottomPin)==LOW && digitalRead(topPin)==LOW)
   {
-    myservo.write(90);
+    myservo.write(90); //turn the tap open
   }
   if(digitalRead(bottomPin)==HIGH && digitalRead(topPin)==HIGH)
   {
-    myservo.write(0);
-  }
-  if(digitalRead(bottomPin)==HIGH && digitalRead(topPin)==LOW)
-  {
-    myservo.write(90);
+    myservo.write(0); //turn the tap close
   }
   
   //delay(60*1000);
