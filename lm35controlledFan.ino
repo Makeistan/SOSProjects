d4ab5f0e92c6fc3d89ac9709bc01954ecc8e0047
Umar Shehzad <umarshehzad@hotmail.com>
2016-12-20 20:20:56 +0500

    lm35 controlled fan code
    lm35 values are mapped from 50 and 220 on analog input to 50 and 255 analog output. may need to calibrate it according to the room

---
+++ lm35controlledFan.ino
@@ -0,0 +1,26 @@
+int lm35Pin=A5;
+int motorPin=10;
+int lm35Val=0;
+int motorVal=0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(lm35Pin,INPUT);
+  pinMode(motorPin, OUTPUT);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  lm35Val=analogRead(lm35Pin);
+  if(lm35Val>=50)
+  motorVal=map(lm35Val, 50, 220, 50, 255);
+  else
+  motorVal=0;
+  Serial.println(lm35Val);
+  analogWrite(motorPin,motorVal);
+
+  
+
+}
